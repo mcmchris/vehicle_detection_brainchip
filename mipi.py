4913e5f430abc918dc3536f538badc3cb1ece66c
Christopher Méndez <mcmchris@hotmail.com>
2024-05-09 17:12:21 -0400

    mipi ex added

--- mipi.py
+++ mipi.py
@@ -195,7 +195,7 @@ def inferencing(model_file, queueOut):
             img = cv2.circle(img, (int((bb['x'] + int(bb['width']/2)) * scale_out_x), int((bb['y'] + int(bb['height']/2)) * scale_out_y)), 8, (57, 255, 20), 2)
             img = cv2.circle(img, (int((bb['x'] + int(bb['width']/2)) * scale_out_x), int((bb['y'] +  int(bb['height']/2)) * scale_out_y)), 4, (255, 165, 0), 2)
 
-        img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+        #img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
 
         if not queueOut.full():
             queueOut.put(img)
